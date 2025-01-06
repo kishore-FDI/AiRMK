ac4dc19165710b39eba6950227826672b272013c
Kishore <kishore22705@gmail.com>
2025-01-06 11:38:11 +0530

    Test

--- frontend/app/api/route.tsx
+++ frontend/app/api/route.tsx
@@ -1,6 +1,6 @@
 import { NextResponse } from "next/server";
-
-const BACKEND_URL = process.env.HOST;
+import {SERVER} from "./server.json"
+const BACKEND_URL = SERVER;
 
 export async function POST(req:any) {
   try {
